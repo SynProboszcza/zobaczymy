829f7455a009f03310caa205a7cb03f10ab2aefb
SynProboszcza <SynProboszcza>
2021-02-22 14:02:29 +0100

    zad1

---
+++ venv/Scripts/zad1.py
@@ -0,0 +1,18 @@
+#Zad1. Napisz pierwszy skrypt, w którym zadeklarujesz po dwie zmienne każdego typu a następnie wyświetl te zmienne
+
+int1 = 1
+int2 = 2
+string1 = "Candy"
+string2 = "Strawberry"
+float1 = 15.3
+float2 = 15.15
+complex1 = 1+1j
+complex2 = 12+12j
+
+print(int1, int2, string1, string2, float1, float2, complex1, complex2)
+
+
+
+
+
+
