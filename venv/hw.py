5c0a5fd2579696f5599c81541ecea80fc7007838
SynProboszcza <SynProboszcza>
2021-02-22 13:51:25 +0100

    dalsze zagadnienia

--- venv/hw.py
+++ venv/hw.py
@@ -3,19 +3,26 @@ a = 10
 b = 5
 c = 2
 
-print(a/b)
-print(a//b)
-print(a+c)
-print(a**b)
-print(pow(b,c))
-print(sqrt(9))
-print(pi)
+print(a/b)		#dzielenie
+print(a//b)		#dzielenie ale zwróci int
+print(a+c)		#dodawanie
+print(a**b)		#potęgowanie
+print(pow(b,c))	#potęgowanie
+print(sqrt(9))	#pierwiastek
+print(pi)		#jakaś tam stała naukowa
 
 
+a+=4
+print(a)
 
+imie = "Alicja"
+wiek = 20
 
+print("Siedzi obok mnie %s i ma %d lat" % (imie, wiek))
 
+z = 15 - 13
 
+print("Wynik działania %(z1)d-%(z2)d=%(z3)d" % {'z1': a, 'z2': b, 'z3': z})
 
 
 
